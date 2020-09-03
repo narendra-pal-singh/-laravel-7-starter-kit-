5ccee822c1f634c84e882329d601a1e9ec0f8bd4
npsingh <npsingh.sb@gmail.com>
2020-09-03 06:09:34 +0530

    changed heading

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Starter Kit Stage 1
                 </div>
 
                 <div class="links">
